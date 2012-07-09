359d9c4a45c450238ea6b3b2ee3e2a9efeee434f
Fabio Zadrozny <Fabio Zadrozny>
2012-07-09 15:48:23 -0300

    Comments on SciTE.

--- mu_repo/__references__.py
+++ mu_repo/__references__.py
@@ -23,3 +23,42 @@ git unstage -i= interactively unstage files
 
 git ignore
 '''
+
+#--------------------------------------------------------------------------------------------- SCITE
+'''
+Using SciTE:
+
+Grab it from: http://www.scintilla.org/SciTEDownload.html
+Put contents in SciTE.properties or %USERPROFILE%\SciTEUser.properties
+
+Configuration:
+#Edited from instructions on: http://www.scintilla.org/SciTEDoc.html
+
+#Monospaced
+font.base=$(font.monospace)
+font.small=$(font.monospace)
+font.comment=$(font.monospace)
+font.text=$(font.monospace)
+font.text.comment=$(font.monospace)
+font.embedded.base=$(font.monospace)
+font.embedded.comment=$(font.monospace)
+font.vbs=$(font.monospace)
+position.maximize=true
+
+#Line numbers
+line.margin.visible=1
+line.margin.width=3+
+
+#Better antialiasing on Vista onwards.
+technology=1
+
+#With git, always work with LF!
+eol.mode=LF
+
+#These are here but they only work in %USERPROFILE%\SciTEUser.properties!
+position.left=300
+position.top=100
+position.width=800
+position.height=600
+position.maximize=0
+'''
