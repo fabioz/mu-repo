c2bc608efd5977687b7dfcce34dd4710f541d01d
Fabio Zadrozny <fabioz@esss.com.br>
2014-06-30 09:22:40 -0300

    If a directory is passed to register ending with / or \ we strip that part and use only the directory name.

--- mu_repo/action_register.py
+++ mu_repo/action_register.py
@@ -27,22 +27,29 @@ def Run(params):
 
         args = [repo for repo in os.listdir('.') if isdir(join(repo, '.git'))]
 
+    new_args = []
+    for arg in args:
+        if arg.endswith('\\') or arg.endswith('/'):
+            arg = arg[:-1]
+        new_args.append(arg)
+    args = new_args
+
     group_repos = config.groups.get(config.current_group, None)
-    
+
     for repo in args:
         if repo in repos:
             msg = 'Repository: %s skipped, already registered' % (repo,)
         else:
             repos.append(repo)
             msg = 'Repository: %s registered' % (repo,)
-            
+
         if group_repos is not None:
             if repo not in group_repos:
                 group_repos.append(repo)
                 msg += ' (added to group "%s")' % config.current_group
             else:
                 msg += ' (already in group "%s")' % config.current_group
-                
+
         Print(msg)
         msgs.append(msg)
 
