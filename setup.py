93faaa3a61507cafc38343130ab737515296ec2b
Fabio Zadrozny <fabioz@esss.com.br>
2015-04-29 11:25:50 -0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='mu_repo',
+    version='1.0.0',
+    description = 'Tool to work with multiple git repositories',
+    author='Fabio Zadrozny',
+    url='https://github.com/fabioz/mu-repo',
+    scripts=['mu'],
+    packages=['mu_repo'],
+)
