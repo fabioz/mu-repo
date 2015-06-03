05df666ed9bce76f1e7109a66d63238e8c0fbbb6
Fabio Zadrozny <fabioz@esss.com.br>
2015-06-03 09:39:20 -0300

    Generating page at proper location.

--- mkdocs_sources/build_mkdocs.py
+++ mkdocs_sources/build_mkdocs.py
@@ -13,10 +13,9 @@ Note: to install mkdocs: pip install mkdocs
 '''
 
 import os
+import shutil
 import subprocess
 import sys
-
-
 
 
 subprocess.call([
@@ -24,3 +23,16 @@ subprocess.call([
     'build'],
     cwd=os.path.dirname(__file__),
 )
+
+target_dir = os.path.dirname(os.path.dirname(__file__))
+site_dir = os.path.join(target_dir, 'site')
+
+for f in os.listdir(site_dir):
+    if os.path.isdir(os.path.join(site_dir, f)):
+        if os.path.exists(os.path.join(target_dir, f)):
+            shutil.rmtree(os.path.join(target_dir, f))
+
+        shutil.copytree(os.path.join(site_dir, f), os.path.join(target_dir, f))
+    else:
+        shutil.copy(os.path.join(site_dir, f), os.path.join(target_dir, f))
+        
