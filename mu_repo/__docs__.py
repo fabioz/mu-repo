50e223a3bc7cf7774bf00752594768bb6f12958e
Fabio Zadrozny <fabiofz@gmail.com>
2016-05-19 08:32:58 -0300

    mu up -a|--all makes a fetch which is always in parallel (getting all branches, unlike mu up which will get the current branch in parallel).

--- mu_repo/__docs__.py
+++ mu_repo/__docs__.py
@@ -10,7 +10,7 @@
 * ${START_COLOR}mu set-var serial=0|1:${RESET_COLOR} Set commands to be executed serially or in parallel.
 * ${START_COLOR}mu get-vars:${RESET_COLOR} Prints the configuration file.
 * ${START_COLOR}mu fix-eol:${RESET_COLOR} Changes end of lines to '\\n' on all changed files.
-* ${START_COLOR}mu find-branch [-r] *pat*:${RESET_COLOR} 
+* ${START_COLOR}mu find-branch [-r] *pat*:${RESET_COLOR}
     Finds all branches matching a given pattern (or simply mu fb).
 * ${START_COLOR}mu git-init-config:${RESET_COLOR} Initial git configuration (username, log, etc.)
 * ${START_COLOR}mu auto-update:${RESET_COLOR} Automatically updates mu-repo
@@ -28,14 +28,14 @@
 
 * ${START_COLOR}mu clone:${RESET_COLOR} Cloning multiple repos from a base url.
   Use mu clone --help to open browser with more details.
-  
+
 * ${START_COLOR}mu <command> repo:<repo1>,<repo2>${RESET_COLOR}
    Allows specifying target repositories for a single command:
    e.g.: ${START_COLOR}mu st repo:repo1,repo2${RESET_COLOR}: Will do st on repo1 and repo2.
 
 * ${START_COLOR}mu group:${RESET_COLOR} Repository grouping
 
-  * ${START_COLOR}mu group add <name> [--empty]:${RESET_COLOR} 
+  * ${START_COLOR}mu group add <name> [--empty]:${RESET_COLOR}
       Creates new group with current repositories, unless --empty is given
   * ${START_COLOR}mu group rm <name>:${RESET_COLOR} Removes a group
   * ${START_COLOR}mu group switch <name>:${RESET_COLOR} Switches to an existing group
@@ -44,13 +44,14 @@
 
   Use ${START_COLOR}mu register${RESET_COLOR} normally to add repositories to the current group
   Use ${START_COLOR}mu list${RESET_COLOR} to list repositories in the current group
-  
+
 Shortcuts:
 
 ${START_COLOR}mu st         ${RESET_COLOR}= Nice status message for all repos (always in parallel)
 ${START_COLOR}mu co branch  ${RESET_COLOR}= git checkout branch
 ${START_COLOR}mu mu-branch  ${RESET_COLOR}= git rev-parse --abbrev-ref HEAD (print current branch)
 ${START_COLOR}mu up         ${RESET_COLOR}= git fetch origin curr_branch:refs/remotes/origin/curr_branch
+${START_COLOR}mu up --all   ${RESET_COLOR}= git fetch origin (always in parallel)
 ${START_COLOR}mu upd | sync ${RESET_COLOR}= up/diff incoming changes
 ${START_COLOR}mu a          ${RESET_COLOR}= git add -A
 ${START_COLOR}mu c msg      ${RESET_COLOR}= git commit -m "Message" (the message must always be passed)
--- mu_repo/action_up.py
+++ mu_repo/action_up.py
@@ -14,14 +14,17 @@ from mu_repo.print_ import Print, CreateJoinedReposMsg
 #===================================================================================================
 def Run(params):
     repos_and_curr_branch = GetReposAndCurrBranch(params)
-
     commands = []
-    for repo, branch in repos_and_curr_branch:
-        #We want to update origin/master and not FETCH_HEAD
-        #See: http://stackoverflow.com/questions/11051761/why-git-fetch-specifying-branch-does-not-match-fetch-without-specifying-branch/
-        commands.append(ParallelCmd(
-            repo, [params.config.git, 'fetch', 'origin', '%s:refs/remotes/origin/%s' % (branch, branch)]))
+    if len(params.args) > 1 and params.args[1] in ('-a', '--all'):
+        for repo, branch in repos_and_curr_branch:
+            commands.append(ParallelCmd(repo, [params.config.git, 'fetch']))
 
+    else:
+        for repo, branch in repos_and_curr_branch:
+            #We want to update origin/master and not FETCH_HEAD
+            #See: http://stackoverflow.com/questions/11051761/why-git-fetch-specifying-branch-does-not-match-fetch-without-specifying-branch/
+            commands.append(ParallelCmd(
+                repo, [params.config.git, 'fetch', 'origin', '%s:refs/remotes/origin/%s' % (branch, branch)]))
 
 
     repos = []
